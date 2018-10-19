61583bc67877ed6749b4e410530ff7bb3e419587
Vadim <gabderahmanov99@gmail.com>
2018-10-19 21:32:04 +0300

    add fnction print in lab9

--- lab№9/main.c
+++ lab№9/main.c
@@ -3,56 +3,66 @@
 
 int main()
 {
-// *** Variables ***
 
 int** array;
-int i,j;
+int i,j, result = 0;
 int RowCount,ColCount;
 
-// *** Program code ***
 
-// Input size of array
 printf("Enter count of row: ");
 scanf("%d",&RowCount);
 printf("Enter count of col: ");
 scanf("%d",&ColCount);
 
-// Print entered size
 printf("Array size: %dx%d\n",RowCount,ColCount);
 
-// Create array of int* - allocate memory
 array= (int **)malloc(sizeof(int*)*RowCount);
 
-// for every pointer to int
 for (i=0;i<RowCount;i++){
-  // Allocate memory for array line of integers
   array[i]=(int *)malloc(sizeof(int)*ColCount);
-  // Get array numbers
+
   for(j=0;j<ColCount;j++){
+
     printf("Enter Array [%d,%d]: ",i,j);
-    // Enter all the integers in array
     scanf("%d",&array[i][j]);
   };
 };
 
-for(i = 0;i<RowCount;i++){
-    for(j=0;j<ColCount;j++){
-        printf("%d\t", array[i][j]);
-    }
-    printf("\n");
-}
+print_(array, RowCount, ColCount);
 
-printf("%d", summ_elements_array(array, RowCount, ColCount));
+result = summ_elements_array(array,RowCount,ColCount);
+printf("%d", result);
+
+for (i=0;i<RowCount;i++){
+  //Clear memory used by lines
+  array[i]=(int *)realloc(array[i],0);
+};
+
+//Clear memory
+array=(int **)realloc(array,0);
+
     return 0;
 }
-
+//sum all elements array
 int summ_elements_array(int** array, int row, int col){
-    int i, j, n;
-    int numElements = col*row;
+    int i, j, n = 0;
+    int* arr;
+
+
     for(i=0;i<row;i++){
             for(j=0;j<col;j++){
-               n+=array[i][j];
+               n=n+array[i][j];
             }
     }
     return n;
+}
+//print array
+void print_(int** array, int row, int col){
+    int i, j;
+    for(i = 0;i<row;i++){
+    for(j=0;j<col;j++){
+        printf("%d\t", array[i][j]);
+    }
+   printf("\n");
+}
 }
