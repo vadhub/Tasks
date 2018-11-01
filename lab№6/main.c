4ff471824aa1602429d568fc75b453307bec907d
Vadim <gabderahmanov99@gmail.com>
2018-11-01 13:57:07 +0300

    add function for 3*3 matric

--- lab№6/main.c
+++ lab№6/main.c
@@ -42,40 +42,90 @@ for (i=0;i<RowCount;i++){
   if(RowCount==2&&ColCount==2){
             if(i==0&&j==0){
             /*
-                00
+                10
                 0
             */
             array2[i][j] = (int)(array[i][j]+array[i][j+1]+array[i+1][j])/3;
             }else if(i==0&&j<=ColCount){
             /*
-                00
+                01
                  0
             */
             array2[i][j] = (int)(array[i][j]+array[i][j-1]+array[i+1][j])/3;
             }else if(i<=RowCount&&j==0){
             /*
                 0
-                00
+                10
             */
             array2[i][j] = (int)(array[i][j]+array[i-1][j]+array[i][j+1])/3;
             }else if(i<=RowCount&&j<=ColCount){
             /*
                  0
-                00
+                01
             */
             array2[i][j] = (int)(array[i][j]+array[i-1][j]+array[i][j-1])/3;
             }
         }else if(RowCount>2&&ColCount>2){
             if(i==0&&j==0){
+                /*
+                    10
+                    0
+                */
                 array2[i][j] = (array[i][j]+array[i+1][j]+array[i][j+1])/3;
-            }else if(i==0&&j>0&&j!=ColCount){
-                array2[i][j] = (array[i][j]+array[i][j-1]+array[i][j+1]+array[i+1][j+1])/4;
-            }else if(i>0&&j>0&&i!=RowCount&&j!=ColCount){
+            }else if(i==0&&j>0&&j!=(ColCount-1)){
+                /*
+                    010
+                     0
+                */
+                array2[i][j] = (array[i][j]+array[i][j-1]+array[i][j+1]+array[i+1][j])/4;
+            }else if(i>0&&j>0&&i!=(RowCount-1)&&j!=(ColCount-1)){
+                /*
+                     0
+                    010
+                     0
+                */
                 array2[i][j] = (array[i][j]+array[i][j-1]+array[i-1][j]+array[i][j+1]+array[i+1][j])/5;
-            }else if(i>0&&j==0&&i!=RowCount){
+            }else if(i>0&&j==0&&i!=(RowCount-1)){
+                /*
+                    0
+                    10
+                    0
+                */
                 array2[i][j] = (array[i][j]+array[i+1][j]+array[i-1][j]+array[i][j+1])/4;
-            }
+            }else if(i==0&&j<=ColCount){
+                /*
+                    01
+                     0
+
+                */
+                array2[i][j] = (array[i][j]+array[i][j-1]+array[i+1][j])/3;
+            }else if(i<=RowCount&&j==0){
+                /*
+                    0
+                    10
+                */
+                array2[i][j] = (array[i][j]+array[i-1][j]+array[i][j+1])/3;
+            }else if(i<=RowCount&&j>0&&j!=(ColCount-1)){
+                /*
+                     0
+                    010
+                */
+                array2[i][j] = (array[i][j]+array[i-1][j]+array[i][j+1]+array[i][j-1])/4;
+            }else if(i<=RowCount&&j<=ColCount){
+                /*
+                      0
+                     01
+                */
+                array2[i][j] = (array[i][j]+array[i-1][j]+array[i][j-1])/3;
+            }else if(i<=RowCount&&j<=ColCount){
+                /*
+                      0
+                     01
+                      0
+                */
+                array2[i][j] = (int)((array[i][j]+array[i-1][j]+array[i][j-1]+array[i+1][j])/4);
         }
+    }
   };
 };
 
