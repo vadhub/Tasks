c17f108db8c21a9078b57fbf21d1332c782d8f16
Vadim <gabderahmanov99@gmail.com>
2018-11-02 17:08:41 +0300

    correct method of sum array

--- lab№9/main.c
+++ lab№9/main.c
@@ -5,30 +5,18 @@ int main()
 {
 
 int** array;
-int i,j, result = 0;
-int RowCount,ColCount;
+int i,j;
+int size;
 
-printf("Enter count of row: ");
-scanf("%d",&RowCount);
-printf("Enter count of col: ");
-scanf("%d",&ColCount);
+printf("Enter size of matric: ");
+scanf("%d",&size);
 
-printf("Array size: %dx%d\n",RowCount,ColCount);
-
-
-for (i=0;i<RowCount;i++){
-  //Clear memory used by lines
-  array[i]=(int *)realloc(array[i],0);
-};
-
-//Clear memory
-array=(int **)realloc(array,0);
-
+summ_elements_array(array, size);
     return 0;
 }
 //sum all elements array
 void summ_elements_array(int** array, int size){
-    int i, j, n;
+    int i, j, n = 0;
     srand(time(0));
     array = (int **)malloc(size*sizeof(int*));
     for(i=0;i<size;i++){
@@ -37,35 +25,41 @@ void summ_elements_array(int** array, int size){
              array[i][j] = rand()%10;
         }
     }
-    int x;
-    for(i=0;i<size-1;i++){
-        for(j=0;j<size-1;j++){
-            x=array[i][j];
-            array[i][j]=array[size-j+1][size-i+1];
-            array[size-j+1][size-i+1]=x;
+    for(i=0;i<size;i++){
+        for(j=0;j<size;j++){
+            array[i][j]=array[j][i];
         }
     }
 
+    print_(array,size);
+
+    for(i=0;i<size;i++){
+        for(j=0;j<size;j++){
+            n=n+array[i][j];
+        }
+    }
+
+    printf("sum elements = %d", n);
 
 
-    for (i=0;i<RowCount;i++){
+    for (i=0;i<size;i++){
     //Clear memory used by lines
         array[i]=(int *)realloc(array[i],0);
     };
 
     //Clear memory
     array=(int **)realloc(array,0);
-     /*for(i=0;i<size;i++){
+     for(i=0;i<size;i++){
         for(j=0;j<size;j++){
-               n=n+array[i][j];
+            n=n+array[i][j];
         }
-    }*/
+    }
 }
 //print array
-void print_(int** array, int row, int col){
+void print_(int** array, int size){
     int i, j;
-    for(i = 0;i<row;i++){
-        for(j=0;j<col;j++){
+    for(i = 0;i<size;i++){
+        for(j=0;j<size;j++){
             printf("%d\t", array[i][j]);
         }
         printf("\n");
